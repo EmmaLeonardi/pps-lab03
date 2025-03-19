7b286f0ef9bbc5b15c3a87484bd774961a0b7040
Emma Leonardi <eleonardi.mobile@gmail.com>
2025-03-19 10:38:35 +0100

    Implemented count all teacher courses

--- src/main/scala/u02/Lab03.scala
+++ src/main/scala/u02/Lab03.scala
@@ -12,12 +12,22 @@ object Lab03 extends App {
   def allTeacherCourses(s: Sequence[Person]): Sequence[String] = flatMap(s)(_ match
     case Teacher(_, c) => Cons(c, Nil())
     case Student(_, _) => Nil())
+
+
+  @tailrec
+  def foldLeft[A, B](l: Sequence[A])(defaultValue: B)(f: (B, A) => B): B = l match
+    case Nil() => defaultValue
+    case Cons(h, t) => foldLeft(t)(f(defaultValue, h))(f)
+
+  def countAllTeacherCourses(s: Sequence[Person]): Int = s match
+    case Nil() => 0
+    case Cons(h, t) => h match
+      case Teacher(_, _) => 1 + countAllTeacherCourses(t)
+      case Student(_, _) => countAllTeacherCourses(t)
+
 }
 
-@tailrec
-def foldLeft[A, B](l: Sequence[A])(defaultValue: B)(f: (B, A) => B): B = l match
-  case Nil() => defaultValue
-  case Cons(h, t) => foldLeft(t)(f(defaultValue, h))(f)
+
 
 
 
--- src/test/scala/u02/Lab03Test.scala
+++ src/test/scala/u02/Lab03Test.scala
@@ -2,7 +2,7 @@ package u02
 
 import org.junit.*
 import org.junit.Assert.*
-import u02.Lab03.allTeacherCourses
+import u02.Lab03.*
 import u02.Modules.Person.{Student, Teacher}
 import u03.Sequences.Sequence.{Cons, Nil}
 
@@ -13,18 +13,25 @@ class Lab03Test:
   val teacher1 = Teacher("Mario", course1)
   val teacher2 = Teacher("Antonio", course2)
   val student = Student("Luigi", 2025)
+  val list = Cons(teacher1, Cons(student, Cons(teacher2, Nil())))
 
   //Task 2 - svolto da sola
   @Test def testAllTeacherCoursesEmptyList() =
     assertEquals(allTeacherCourses(Nil()), Nil())
 
   @Test def testAllTeacherCoursesWithStudents() =
-    assertEquals(allTeacherCourses(Cons(teacher1, Cons(student, Cons(teacher2, Nil())))),
+    assertEquals(allTeacherCourses(list),
       Cons(course1, Cons(course2, Nil())))
 
   @Test def testFoldLeft() =
     val lst = Cons(3, Cons(7, Cons(1, Cons(5, Nil()))))
     assertEquals(foldLeft(lst)(0)(_ - _), -16)
     assertEquals(foldLeft(Cons(0, Nil()))(0)(_ - _), 0)
+
+  @Test def testCountTeacherCoursesEmptyList() =
+    assertEquals(countAllTeacherCourses(Nil()), 0)
+
+  @Test def testCountTeacherCourseWithStudents() =
+    assertEquals(countAllTeacherCourses(list), 2)
 
 end Lab03Test
